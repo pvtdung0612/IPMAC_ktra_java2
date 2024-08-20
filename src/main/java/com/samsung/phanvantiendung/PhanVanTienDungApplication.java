ff94d17158f55f673ba150395b158ec043d550f5
pvtdung0612 <pvtdung0612@gmail.com>
2024-08-20 14:16:48 +0700

    init prj

---
+++ src/main/java/com/samsung/phanvantiendung/PhanVanTienDungApplication.java
@@ -0,0 +1,13 @@
+package com.samsung.phanvantiendung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhanVanTienDungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhanVanTienDungApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samsung/phanvantiendung/PhanVanTienDungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samsung.phanvantiendung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhanVanTienDungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
